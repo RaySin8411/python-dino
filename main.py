c086556358d6ce7aa535abbedcc70acca087a102
raysin8411 <q8977452@gmail.com>
2026-02-14 22:41:18 +0800

    dino in practice for running, jumping and ducking

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ screen_width = 1100
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+RUNNING = [ 
+	pygame.image.load(os.path.join("assets/Dino", "Chrome Dino Run.png")), 
+	pygame.image.load(os.path.join("assets/Dino", "Chrome Dino Run 2.png")) 
+] 
+JUMPING = pygame.image.load(os.path.join("assets/Dino", "Dino Jump.png")) 
+DUCKING = [ 
+	pygame.image.load(os.path.join("assets/Dino", "Dino Duck.png")), 
+	pygame.image.load(os.path.join("assets/Dino", "Dino Duck 2.png")) 
+]
