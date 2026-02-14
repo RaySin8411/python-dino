8f4fe27a9272b9f212dd7578204040eb5ee1866c
raysin8411 <q8977452@gmail.com>
2026-02-14 22:53:25 +0800

    define other class, for example brid LargeCactus, and SmallCactus etc.

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ class Dinosaur:
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
 
+
 CLOUD = pygame.image.load(os.path.join("assets/Other", "Chrome Dinosaur Cloud.png"))
 
 class Cloud:
@@ -111,3 +112,75 @@ class Cloud:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.x, self.y))
+
+
+BG = pygame.image.load(os.path.join("assets/Other", "Chrome Dinosaur Track.png"))
+
+class Obstacle:
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = screen_width
+        
+    def update(self):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+        
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
+
+
+SMALL_CACTUS = [
+    pygame.image.load(
+        os.path.join("assets/Cactus", "Chrome Dinosaur Small Cactus.png")),
+    pygame.image.load(
+        os.path.join("assets/Cactus", "Chrome Dinosaur Small Cactus (1).png")),
+    pygame.image.load(
+        os.path.join("assets/Cactus", "Chrome Dinosaur Small Cactus (2).png"))
+]
+
+class SmallCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+
+
+LARGE_CACTUS = [
+    pygame.image.load(
+        os.path.join("assets/Cactus", "Chrome Dinosaur Large Cactus.png")),
+    pygame.image.load(
+        os.path.join("assets/Cactus", "Chrome Dinosaur Large Cactus (1).png")),
+    pygame.image.load(
+        os.path.join("assets/Cactus", "Chrome Dinosaur Large Cactus (2).png")),
+]
+
+class LargeCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 300
+
+
+BIRD = [
+    pygame.image.load(
+        os.path.join("assets/Bird", "Chrome Dinosaur Bird1.png")),
+    pygame.image.load(
+        os.path.join("assets/Bird", "Chrome Dinosaur Bird2.png")),
+]
+
+class Bird(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 250
+        self.index = 0
+
+    def draw(self, SCREEN):
+        if self.index >= 10:
+            self.index = 0
+        SCREEN.blit(self.image[self.index//5], self.rect)
+        self.index += 1
+
